6beeb5545f1cdc41c3308a651dc57fb0df931d58
Avo <avomandjian@gmail.com>
2020-07-03 02:16:47 +0300

    basicOfTheApp

---
+++ android/app/src/main/kotlin/com/example/catalogue_my_version/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.catalogue_my_version
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stones'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'Purple',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 200.0,
+                child: ListView.builder(
+                  physics: ClampingScrollPhysics(),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 15,
+                  itemBuilder: (BuildContext context, int index) => Card(
+                    child: Center(child: Text('Dummy Card Text')),
+                  ),
+                ),
+              ),
+              Text(
+                'Red',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 200.0,
+                child: ListView.builder(
+                  physics: ClampingScrollPhysics(),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 15,
+                  itemBuilder: (BuildContext context, int index) => Card(
+                    child: Center(child: Text('Dummy Card Text')),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
