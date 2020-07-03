db65ea982ea17d47c9c46db5a153e21c5d0d3b00
Avo <avomandjian@gmail.com>
2020-07-03 14:14:15 +0300

    Refactored Code

--- lib/main.dart
+++ lib/main.dart
@@ -19,136 +19,89 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Text(
-                'Red',
-                style: TextStyle(fontSize: 18),
+              TextCustom(
+                category: 'red',
               ),
-              SizedBox(
-                height: 200.0,
-                child: FutureBuilder(
-                  future: DefaultAssetBundle.of(context)
-                      .loadString('assets/JsonFiles/Red.json'),
-                  builder: (context, snapshot) {
-                    var myData = json.decode(snapshot.data.toString());
-                    return ListView.builder(
-                      physics: ClampingScrollPhysics(),
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: myData == null ? 0 : myData.length,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () => showModalBottomSheet(
-                              context: context,
-                              builder: (context) {
-                                return SingleChildScrollView(
-                                  child: Column(
-                                    children: [
-                                      Text(myData[index]['name']),
-                                      Image.asset(
-                                        myData[index]['image'],
-                                        height: 300,
-                                      ),
-                                      Text(myData[index]['description'])
-                                    ],
-                                  ),
-                                );
-                              }),
-                          child: Card(
-                            child: Image.asset(
-                              myData[index]['image'],
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
+              buildFutureBuilder(context, jsonFileName: 'Red'),
+              TextCustom(
+                category: 'Purple',
               ),
-              Text(
-                'Purple',
-                style: TextStyle(fontSize: 18),
+              buildFutureBuilder(context, jsonFileName: 'Purple'),
+              TextCustom(
+                category: 'Green',
               ),
-              SizedBox(
-                height: 200.0,
-                child: FutureBuilder(
-                  future: DefaultAssetBundle.of(context)
-                      .loadString('assets/JsonFiles/Purple.json'),
-                  builder: (context, snapshot) {
-                    var myData = json.decode(snapshot.data.toString());
-                    return ListView.builder(
-                      physics: ClampingScrollPhysics(),
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: myData == null ? 0 : myData.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: Image.asset(
-                            myData[index]['image'],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
+              buildFutureBuilder(context, jsonFileName: 'Green'),
+              TextCustom(
+                category: 'Blue',
               ),
-              Text(
-                'Green',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(
-                height: 200.0,
-                child: FutureBuilder(
-                  future: DefaultAssetBundle.of(context)
-                      .loadString('assets/JsonFiles/Green.json'),
-                  builder: (context, snapshot) {
-                    var myData = json.decode(snapshot.data.toString());
-                    return ListView.builder(
-                      physics: ClampingScrollPhysics(),
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: myData == null ? 0 : myData.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: Image.asset(
-                            myData[index]['image'],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ),
-              Text(
-                'Blue',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(
-                height: 200.0,
-                child: FutureBuilder(
-                  future: DefaultAssetBundle.of(context)
-                      .loadString('assets/JsonFiles/Blue.json'),
-                  builder: (context, snapshot) {
-                    var myData = json.decode(snapshot.data.toString());
-                    return ListView.builder(
-                      physics: ClampingScrollPhysics(),
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: myData == null ? 0 : myData.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: Image.asset(
-                            myData[index]['image'],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ),
+              buildFutureBuilder(context, jsonFileName: 'Blue'),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  SizedBox buildFutureBuilder(BuildContext context,
+      {@required String jsonFileName}) {
+    return SizedBox(
+      height: 200.0,
+      child: FutureBuilder(
+        future: DefaultAssetBundle.of(context)
+            .loadString('assets/JsonFiles/$jsonFileName.json'),
+        builder: (context, snapshot) {
+          var myData = json.decode(snapshot.data.toString());
+          return ListView.builder(
+            physics: ClampingScrollPhysics(),
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: myData == null ? 0 : myData.length,
+            itemBuilder: (context, index) {
+              return gestureDetectorCustom(context, myData, index);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  GestureDetector gestureDetectorCustom(
+      BuildContext context, myData, int index) {
+    return GestureDetector(
+      onTap: () => showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(myData[index]['name']),
+                  Image.asset(
+                    myData[index]['image'],
+                    height: 300,
+                  ),
+                  Text(myData[index]['description'])
+                ],
+              ),
+            );
+          }),
+      child: Card(
+        child: Image.asset(
+          myData[index]['image'],
+        ),
+      ),
+    );
+  }
+}
+
+class TextCustom extends StatelessWidget {
+  String category;
+  TextCustom({@required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      category,
+      style: TextStyle(fontSize: 18),
     );
   }
 }
