796cabb940d19f25f02b64fc3f498111f6df3255
Avo <avomandjian@gmail.com>
2020-07-03 03:17:37 +0300

    addedFunctionality

--- lib/main.dart
+++ lib/main.dart
@@ -13,41 +13,134 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Stones'),
+          centerTitle: true,
         ),
         body: SingleChildScrollView(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               Text(
-                'Purple',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(
-                height: 200.0,
-                child: ListView.builder(
-                  physics: ClampingScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 15,
-                  itemBuilder: (BuildContext context, int index) => Card(
-                    child: Center(child: Text('Dummy Card Text')),
-                  ),
-                ),
-              ),
-              Text(
                 'Red',
                 style: TextStyle(fontSize: 18),
               ),
               SizedBox(
                 height: 200.0,
-                child: ListView.builder(
-                  physics: ClampingScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 15,
-                  itemBuilder: (BuildContext context, int index) => Card(
-                    child: Center(child: Text('Dummy Card Text')),
-                  ),
+                child: FutureBuilder(
+                  future: DefaultAssetBundle.of(context)
+                      .loadString('assets/JsonFiles/Red.json'),
+                  builder: (context, snapshot) {
+                    var myData = json.decode(snapshot.data.toString());
+                    return ListView.builder(
+                      physics: ClampingScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: myData == null ? 0 : myData.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () => showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return Column(
+                                  children: [
+                                    Text(myData[index]['name']),
+                                    Image.asset(
+                                      myData[index]['image'],
+                                      height: 300,
+                                    ),
+                                    Text(myData[index]['description'])
+                                  ],
+                                );
+                              }),
+                          child: Card(
+                            child: Image.asset(
+                              myData[index]['image'],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              Text(
+                'Purple',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 200.0,
+                child: FutureBuilder(
+                  future: DefaultAssetBundle.of(context)
+                      .loadString('assets/JsonFiles/Purple.json'),
+                  builder: (context, snapshot) {
+                    var myData = json.decode(snapshot.data.toString());
+                    return ListView.builder(
+                      physics: ClampingScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: myData == null ? 0 : myData.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Image.asset(
+                            myData[index]['image'],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              Text(
+                'Green',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 200.0,
+                child: FutureBuilder(
+                  future: DefaultAssetBundle.of(context)
+                      .loadString('assets/JsonFiles/Green.json'),
+                  builder: (context, snapshot) {
+                    var myData = json.decode(snapshot.data.toString());
+                    return ListView.builder(
+                      physics: ClampingScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: myData == null ? 0 : myData.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Image.asset(
+                            myData[index]['image'],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              Text(
+                'Blue',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 200.0,
+                child: FutureBuilder(
+                  future: DefaultAssetBundle.of(context)
+                      .loadString('assets/JsonFiles/Blue.json'),
+                  builder: (context, snapshot) {
+                    var myData = json.decode(snapshot.data.toString());
+                    return ListView.builder(
+                      physics: ClampingScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: myData == null ? 0 : myData.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Image.asset(
+                            myData[index]['image'],
+                          ),
+                        );
+                      },
+                    );
+                  },
                 ),
               ),
             ],
